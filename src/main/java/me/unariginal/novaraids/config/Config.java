d6b8fa8153ce11b7579f626388f9566422e6a4ae
SkyNetCloud <mcboxer1201@gmail.com>
2025-03-14 16:07:40 -0400

    Remove DiscordConfig.java System and merge it into Config.java || updated the config.json with new RaidHook Info

--- src/main/java/me/unariginal/novaraids/config/Config.java
+++ src/main/java/me/unariginal/novaraids/config/Config.java
@@ -37,7 +37,7 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.*;
 
-import static me.unariginal.novaraids.config.DiscordConfig.loadDiscordInfo;
+import static me.unariginal.novaraids.utils.WebhookHandler.*;
 
 public class Config {
     private final NovaRaids nr = NovaRaids.INSTANCE;
@@ -58,7 +58,6 @@ public class Config {
         }
 
         try {
-            loadDiscordInfo();
             loadConfig();
             loadMessages();
             loadBossBars();
@@ -230,6 +229,12 @@ public class Config {
         JsonArray banned_abilities = getSafe(banned_section, "banned_abilities", "Json String Array", "config.json/raid_settings/banned_section").getAsJsonArray();
         JsonArray banned_held_items = getSafe(banned_section, "banned_held_items", "Json String Array", "config.json/raid_settings/banned_section").getAsJsonArray();
         JsonArray banned_bag_items = getSafe(banned_section, "banned_bag_items", "Json String Array", "config.json/raid_settings/banned_section").getAsJsonArray();
+
+        raid_webhook_toggle = getSafe(config, "raid_webhook_toggle", "boolean", "discord.json").getAsBoolean();
+        raid_webhookurl = getSafe(config, "raid_webhookurl", "String", "discord.json").getAsString();
+        raid_title = getSafe(config, "raid_title", "String", "discord.json").getAsString();
+        raid_role_at = getSafe(config, "raid_role_at", "String", "discord.json").getAsString();
+        raid_avatarurl = getSafe(config, "raid_avatarurl", "String", "discord.json").getAsString();
 
         List<Species> banned_pokemon_list = new ArrayList<>();
         for (JsonElement element : banned_pokemon) {
