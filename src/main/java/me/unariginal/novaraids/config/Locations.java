b61d0e37b6741420a8d22d6e09c8878e8dbcf3a8
BuildTools <unconfigured@null.spigotmc.org>
2025-04-06 14:49:51 -0400

    New locations.json

---
+++ src/main/java/me/unariginal/novaraids/config/Locations.java
@@ -0,0 +1,138 @@
+package me.unariginal.novaraids.config;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import me.unariginal.novaraids.NovaRaids;
+import me.unariginal.novaraids.data.Location;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.Vec3d;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Locations {
+    private final NovaRaids nr = NovaRaids.INSTANCE;
+
+    List<Location> locations = new ArrayList<>();
+
+    public Locations() {
+        try {
+            loadLocations();
+        } catch (IOException | NullPointerException | UnsupportedOperationException e) {
+            nr.logError("[RAIDS] Failed to load locations file.");
+        }
+    }
+
+    public void loadLocations() throws IOException, NullPointerException, UnsupportedOperationException {
+        File rootFolder = FabricLoader.getInstance().getConfigDir().resolve("NovaRaids").resolve("config").toFile();
+        if (!rootFolder.exists()) {
+            rootFolder.mkdirs();
+        }
+
+        File file = FabricLoader.getInstance().getConfigDir().resolve("NovaRaids/locations.json").toFile();
+        if (file.createNewFile()) {
+            InputStream stream = NovaRaids.class.getResourceAsStream("/raid_config_files/locations.json");
+            assert stream != null;
+            OutputStream out = new FileOutputStream(file);
+
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = stream.read(buffer)) > 0) {
+                out.write(buffer, 0, length);
+            }
+
+            stream.close();
+            out.close();
+        }
+
+        JsonElement root = JsonParser.parseReader(new FileReader(file));
+        assert root != null;
+        JsonObject config = root.getAsJsonObject();
+
+        List<Location> locations = new ArrayList<>();
+        for (String key : config.keySet()) {
+            JsonObject location_object = config.getAsJsonObject(key);
+            double x = 0;
+            double y = 100;
+            double z = 0;
+            ServerWorld world = nr.server().getOverworld();
+            int border_radius = 30;
+            int boss_pushback_radius = 5;
+            float boss_facing_direction = 0;
+            boolean use_join_location = false;
+            double join_x = 0;
+            double join_y = 100;
+            double join_z = 7;
+            float yaw = 0;
+            float pitch = 0;
+
+            if (checkProperty(location_object, "x_pos")) {
+                x = location_object.get("x_pos").getAsDouble();
+            }
+            if (checkProperty(location_object, "y_pos")) {
+                y = location_object.get("y_pos").getAsDouble();
+            }
+            if (checkProperty(location_object, "z_pos")) {
+                z = location_object.get("z_pos").getAsDouble();
+            }
+            Vec3d pos = new Vec3d(x, y, z);
+
+            if (checkProperty(location_object, "world")) {
+                String world_path = location_object.get("world").getAsString();
+                for (ServerWorld w : nr.server().getWorlds()) {
+                    if (w.getRegistryKey().toString().equals(world_path)) {
+                        world = w;
+                        break;
+                    }
+                }
+            }
+
+            if (checkProperty(location_object, "border_radius")) {
+                border_radius = location_object.get("border_radius").getAsInt();
+            }
+            if (checkProperty(location_object, "boss_pushback_radius")) {
+                boss_pushback_radius = location_object.get("boss_pushback_radius").getAsInt();
+            }
+            if (checkProperty(location_object, "boss_facing_direction")) {
+                boss_facing_direction = location_object.get("boss_facing_direction").getAsFloat();
+            }
+            if (checkProperty(location_object, "use_join_location")) {
+                use_join_location = location_object.get("use_join_location").getAsBoolean();
+            }
+            if (use_join_location) {
+                if (checkProperty(location_object, "join_location")) {
+                    JsonObject join_location_object = location_object.get("join_location").getAsJsonObject();
+                    if (checkProperty(join_location_object, "x_pos")) {
+                        join_x = join_location_object.get("x_pos").getAsDouble();
+                    }
+                    if (checkProperty(join_location_object, "y_pos")) {
+                        join_y = join_location_object.get("y_pos").getAsDouble();
+                    }
+                    if (checkProperty(join_location_object, "z_pos")) {
+                        join_z = join_location_object.get("z_pos").getAsDouble();
+                    }
+                    if (checkProperty(join_location_object, "yaw")) {
+                        yaw = join_location_object.get("yaw").getAsFloat();
+                    }
+                    if (checkProperty(join_location_object, "pitch")) {
+                        pitch = join_location_object.get("pitch").getAsFloat();
+                    }
+                }
+            }
+            Vec3d join_pos = new Vec3d(join_x, join_y, join_z);
+            locations.add(new Location(key, pos, world, border_radius, boss_pushback_radius, boss_facing_direction, use_join_location, join_pos, yaw, pitch));
+        }
+        this.locations = locations;
+    }
+
+    public boolean checkProperty(JsonObject section, String property) {
+        if (section.has(property)) {
+            return true;
+        }
+        nr.logError("[RAIDS] Missing " + property + " property in config.json. Using default value(s).");
+        return false;
+    }
+}
--- src/main/java/me/unariginal/novaraids/data/Location.java
+++ src/main/java/me/unariginal/novaraids/data/Location.java
@@ -3,5 +3,14 @@ package me.unariginal.novaraids.data;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.Vec3d;
 
-public record Location(String name, Vec3d pos, ServerWorld world, boolean use_set_join_location, Vec3d join_location, float yaw, float pitch) {
+public record Location(String name,
+                       Vec3d pos,
+                       ServerWorld world,
+                       int border_radius,
+                       int boss_pushback_radius,
+                       float boss_facing_direction,
+                       boolean use_set_join_location,
+                       Vec3d join_location,
+                       float yaw,
+                       float pitch) {
 }
