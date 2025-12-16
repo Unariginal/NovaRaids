517cc0f2f72e62deb25f2eb5d07d3222e996a96b
Unariginal <nateabelson@gmail.com>
2025-12-16 13:14:12 -0500

    Forgot about this thing

--- src/main/java/me/unariginal/novaraids/commands/RaidCommands.java
+++ src/main/java/me/unariginal/novaraids/commands/RaidCommands.java
@@ -564,7 +564,7 @@ public class RaidCommands {
         if (player != null) {
             player.sendMessage(TextUtils.deserialize("<gray><st><b><i>---------<reset><red> Nova Raids <reset><gray><st><b><i>---------"));
             player.sendMessage(TextUtils.deserialize("<gray><b>Author: <reset><white>Unariginal <i>(Ariginal)"));
-            player.sendMessage(TextUtils.deserialize("<gray><b>Version: <reset><white>Beta v0.3.4"));
+            player.sendMessage(TextUtils.deserialize("<gray><b>Version: <reset><white>Beta v0.3.8"));
             player.sendMessage(TextUtils.deserialize("<gray><b><i><u><click:open_url:https://github.com/Unariginal/NovaRaids>Source"));
             player.sendMessage(TextUtils.deserialize("<gray><b><i><u><click:open_url:https://github.com/Unariginal/NovaRaids/wiki>Wiki"));
             player.sendMessage(TextUtils.deserialize("<gray><st><b><i>---------------------------"));
